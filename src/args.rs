7a77a286ce3eae43616625d3bde639a283768897
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2018-06-07 12:16:29 +0200

    First working version

---
+++ src/args.rs
@@ -0,0 +1,177 @@
+extern crate clap;
+extern crate simplelog;
+
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
+use std::str::FromStr;
+
+pub type TcpPort = u16;
+
+#[derive(Debug, Eq, PartialEq, Clone, Copy)]
+pub enum VerbosityLevel {
+    DEBUG = 0,
+    INFO  = 1,
+    WARN  = 2,
+    ERROR = 3
+}
+
+impl Into<simplelog::LevelFilter> for VerbosityLevel {
+    fn into(self) -> simplelog::LevelFilter {
+        match self {
+            VerbosityLevel::DEBUG => simplelog::LevelFilter::Debug,
+            VerbosityLevel::INFO  => simplelog::LevelFilter::Info,
+            VerbosityLevel::WARN  => simplelog::LevelFilter::Warn,
+            VerbosityLevel::ERROR => simplelog::LevelFilter::Error,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+/// Holds the program's runtime configuration
+pub struct RuntimeConfig {
+    pub http_ip: IpAddr,
+    pub http_port: TcpPort,
+    pub model: String,
+    pub alphabet: String,
+    pub lm: String,
+    pub trie: String,
+    pub verbosity_level: VerbosityLevel
+}
+
+pub struct ArgsParser;
+
+impl ArgsParser {
+    fn to_ip_addr(o: Option<&str>) -> IpAddr {
+        let default_ip = IpAddr::V4(Ipv4Addr::from_str("0.0.0.0").unwrap());
+        match o {
+            Some(ip_str) => {
+                if Ipv6Addr::from_str(ip_str).is_ok() {
+                    IpAddr::V6(Ipv6Addr::from_str(ip_str).unwrap())
+                } else if Ipv4Addr::from_str(ip_str).is_ok() {
+                    IpAddr::V4(Ipv4Addr::from_str(ip_str).unwrap())
+                } else {
+                    default_ip
+                }
+            },
+            None => default_ip
+        }
+    }
+
+    fn to_port(o: Option<&str>) -> TcpPort {
+        let default_port = 8080;
+        match o.unwrap_or(default_port.to_string().as_str()).parse::<u16>() {
+            Ok(rv) => rv,
+            Err(_) => default_port
+        }
+    }
+
+    fn to_verbosity_level(occ: u64) -> VerbosityLevel {
+        match occ {
+            0   => VerbosityLevel::ERROR,
+            1   => VerbosityLevel::WARN,
+            2   => VerbosityLevel::INFO,
+            3   => VerbosityLevel::DEBUG,
+            _   => VerbosityLevel::DEBUG
+        }
+    }
+
+    pub fn from_cli() -> RuntimeConfig {
+        let matches = clap::App::new("DeepSpeech Inference Server")
+                              .version("0.1")
+                              .author("<lissyx@lissyx.dyndns.org>")
+                              .about("Running inference from POST-ed RAW PCM.")
+                              .arg(clap::Arg::with_name("http_ip")
+                                   .short("h")
+                                   .long("http_ip")
+                                   .value_name("HTTP_IP")
+                                   .help("IP address to listen on for HTTP")
+                                   .takes_value(true)
+                                   .required(false))
+                              .arg(clap::Arg::with_name("http_port")
+                                   .short("p")
+                                   .long("http_port")
+                                   .value_name("HTTP_PORT")
+                                   .help("TCP port to listen on for HTTP")
+                                   .takes_value(true)
+                                   .required(false))
+                              .arg(clap::Arg::with_name("model")
+                                   .short("m")
+                                   .long("model")
+                                   .value_name("MODEL")
+                                   .help("TensorFlow model to use")
+                                   .takes_value(true)
+                                   .required(true))
+                              .arg(clap::Arg::with_name("alphabet")
+                                   .short("a")
+                                   .long("alphabet")
+                                   .value_name("ALPHABET")
+                                   .help("Alphabet file matching the TensorFlow model to use")
+                                   .takes_value(true)
+                                   .required(true))
+                              .arg(clap::Arg::with_name("lm")
+                                   .short("lm")
+                                   .long("lm")
+                                   .value_name("LM")
+                                   .help("KenLM Language Model to use")
+                                   .takes_value(true)
+                                   .required(true))
+                              .arg(clap::Arg::with_name("trie")
+                                   .short("trie")
+                                   .long("trie")
+                                   .value_name("TRIE")
+                                   .help("KenLM Trie to use")
+                                   .takes_value(true)
+                                   .required(true))
+                              .arg(clap::Arg::with_name("v")
+                                   .short("v")
+                                   .multiple(true)
+                                   .help("Sets the level of verbosity"))
+                              .get_matches();
+
+
+        RuntimeConfig {
+            http_ip:    ArgsParser::to_ip_addr(matches.value_of("http_ip")),
+            http_port:  ArgsParser::to_port(matches.value_of("http_port")),
+            model:      String::from(matches.value_of("model").unwrap()),
+            alphabet:   String::from(matches.value_of("alphabet").unwrap()),
+            lm:         String::from(matches.value_of("lm").unwrap()),
+            trie:       String::from(matches.value_of("trie").unwrap()),
+            verbosity_level: ArgsParser::to_verbosity_level(matches.occurrences_of("v"))
+        }
+    }
+}
+
+#[test]
+fn test_to_ip_addr() {
+    assert_eq!(ArgsParser::to_ip_addr(Some("")), IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)));
+    assert_eq!(ArgsParser::to_ip_addr(Some("239.255.0.1")), IpAddr::V4(Ipv4Addr::new(239, 255, 0, 1)));
+    assert_eq!(ArgsParser::to_ip_addr(Some("1.2.3.4")), IpAddr::V4(Ipv4Addr::new(1, 2, 3, 4)));
+    assert_eq!(ArgsParser::to_ip_addr(Some("::1")), IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1)));
+    assert_eq!(ArgsParser::to_ip_addr(Some("ffx3::1")), IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)));
+    assert_eq!(ArgsParser::to_ip_addr(Some("ff03::1")), IpAddr::V6(Ipv6Addr::new(0xff03, 0, 0, 0, 0, 0, 0, 1)));
+}
+
+#[test]
+fn test_to_port() {
+    assert_eq!(ArgsParser::to_port(Some("xxx")), 8080);
+    assert_eq!(ArgsParser::to_port(Some("8080")), 8080);
+    assert_eq!(ArgsParser::to_port(Some("1234")), 1234);
+}
+
+#[test]
+fn test_to_verbosity_level() {
+    assert_eq!(ArgsParser::to_verbosity_level(0),  VerbosityLevel::ERROR);
+    assert_eq!(ArgsParser::to_verbosity_level(1),  VerbosityLevel::WARN);
+    assert_eq!(ArgsParser::to_verbosity_level(2),  VerbosityLevel::INFO);
+    assert_eq!(ArgsParser::to_verbosity_level(3),  VerbosityLevel::DEBUG);
+    assert_eq!(ArgsParser::to_verbosity_level(4),  VerbosityLevel::DEBUG);
+    assert_eq!(ArgsParser::to_verbosity_level(42), VerbosityLevel::DEBUG);
+}
+
+#[test]
+fn test_args() {
+    let rc = ArgsParser::from_cli();
+
+    assert_eq!(rc.http_ip.to_string(), "0.0.0.0");
+    assert_eq!(rc.http_port.to_string(), "8080");
+    assert_eq!(rc.verbosity_level, VerbosityLevel::ERROR);
+}
---
+++ src/inference.rs
@@ -0,0 +1,161 @@
+extern crate serde;
+
+extern crate deepspeech;
+extern crate audrey;
+extern crate futures;
+
+extern crate bytes;
+
+use self::deepspeech::Model;
+use self::audrey::Format;
+use self::audrey::read::Reader;
+use self::audrey::read::Description;
+use self::bytes::Bytes;
+
+use std::sync::mpsc::{Receiver, SyncSender};
+use std::vec::Vec;
+use std::io::Cursor;
+use std::path::Path;
+
+#[derive(Debug)]
+pub struct RawAudioPCM {
+    pub content: Bytes
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct InferenceData {
+    text: String,
+    confidence: f32
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct InferenceResult {
+    status: String,
+    data: Vec<InferenceData>
+}
+
+const N_CEP :u16 = 26;
+const N_CONTEXT :u16 = 9;
+const BEAM_WIDTH :u16 = 500;
+
+const LM_WEIGHT :f32 = 1.75;
+const WORD_COUNT_WEIGHT :f32 = 1.0;
+const VALID_WORD_COUNT_WEIGHT :f32 = 1.0;
+
+// The model has been trained on this specific
+// sample rate.
+const AUDIO_SAMPLE_RATE :u32 = 16000;
+const AUDIO_CHANNELS :u32    = 1;
+const AUDIO_FORMAT :Format   = Format::Wav;
+
+fn start_model(model: String, alphabet: String, lm: String, trie: String)
+    -> Model
+{
+	let mut m = Model::load_from_files(
+        Path::new(&model),
+        N_CEP,
+        N_CONTEXT,
+        Path::new(&alphabet),
+        BEAM_WIDTH);
+
+	m.enable_decoder_with_lm(
+        Path::new(&alphabet),
+        Path::new(&lm),
+        Path::new(&trie),
+        LM_WEIGHT,
+        WORD_COUNT_WEIGHT,
+        VALID_WORD_COUNT_WEIGHT);
+
+    m
+}
+
+fn ensure_valid_audio(desc: Description) -> bool
+{
+    if desc.format() != AUDIO_FORMAT {
+        error!("Invalid audio format: {:?}", desc.format());
+        return false;
+    };
+
+    if desc.channel_count() != AUDIO_CHANNELS {
+        error!("Invalid number of channels: {}", desc.channel_count());
+        return false;
+    };
+
+    if desc.sample_rate() != AUDIO_SAMPLE_RATE {
+        error!("Invalid sample rate: {}", desc.sample_rate());
+        return false;
+    }
+
+    true
+}
+
+fn inference_result(result: String, status: bool)
+    -> InferenceResult
+{
+    let confidence_value = match status {
+        true  => 1.0,
+        false => 0.0
+    };
+
+    let status_value = match status {
+        true  => "ok".to_string(),
+        false => "ko".to_string()
+    };
+
+    let mut inf_data: Vec<InferenceData> = Vec::new();
+    inf_data.push(InferenceData {
+        confidence: confidence_value,
+        text: result
+    });
+
+    let inf_result = InferenceResult {
+        status: status_value,
+        data: inf_data
+    };
+
+    inf_result
+}
+
+pub fn th_inference(model: String, alphabet: String, lm: String, trie: String, rx_audio: Receiver<RawAudioPCM>, tx_string: SyncSender<InferenceResult>) {
+    info!("Inference thread started");
+    let mut model = start_model(model, alphabet, lm, trie);
+
+    loop {
+        info!("Waiting ...");
+        let inf_result = match rx_audio.recv() {
+            Ok(audio) => {
+                info!("Received message: {:?} bytes", audio.content.len());
+
+                let inf = match Reader::new(Cursor::new(&*audio.content)) {
+                    Ok(mut reader) => {
+                        let desc = reader.description();
+
+                        match ensure_valid_audio(desc) {
+                            true  => {
+                                let audio_buf :Vec<_> = reader.samples().map(|s| s.unwrap()).collect::<Vec<_>>();
+                                let result = model.speech_to_text(&audio_buf, AUDIO_SAMPLE_RATE).unwrap();
+                                inference_result(result, true)
+                            },
+                            false => inference_result("".to_string(), false)
+                        }
+                    },
+
+                    Err(err)   => {
+                        error!("Audrey read error: {:?}", err);
+                        inference_result("".to_string(), false)
+                    }
+                };
+
+                inf
+            },
+
+            Err(err_recv)  => {
+                error!("Error trying to rx.recv(): {:?}", err_recv);
+                inference_result("".to_string(), false)
+            }
+        };
+
+        tx_string.send(inf_result);
+    }
+	
+}
